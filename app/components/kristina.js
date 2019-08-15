1c3389dc6809aef93a18835fd7669b5510f72913
Kristina Taing <kataing12@gmail.com>
2019-08-15 13:20:07 -0700

    [prog] moved header to separate file

--- app/components/kristina.js
+++ app/components/kristina.js
@@ -10,23 +10,14 @@ import {
   TouchableOpacity
 } from 'react-native';
 
+import Header from './header.js';
 import Store from './store.js';
 
 const Kristina = () => {
   return (
     <Fragment>
       <SafeAreaView>
-        <View style={styles.header}>
-          <TouchableOpacity>
-            <Image style={styles.menu} source={require('../icons/sidebar.png')}></Image>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Image style={styles.logo} source={require('../icons/fudFeedLogo.png')}></Image>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Image style={styles.camera} source={require('../icons/camera.jpg')}></Image>
-          </TouchableOpacity>
-        </View>
+        <Header />
         <ScrollView>
           <View style={styles.map}>
             <Text>This is Map</Text>
@@ -42,27 +33,27 @@ const Kristina = () => {
 };
 
 const styles = {
-  header: {
-    height: 50,
-    borderBottomColor: 'gray',
-    borderBottomWidth: 0.5,
-    padding: 10,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  menu: {
-    height: 45,
-    width: 45
-  },
-  logo: {
-    height: 40,
-    width: 40
-  },
-  camera: {
-    height: 35,
-    width: 35
-  },
+  // header: {
+  //   height: 50,
+  //   borderBottomColor: 'gray',
+  //   borderBottomWidth: 0.5,
+  //   padding: 10,
+  //   alignItems: 'center',
+  //   flexDirection: 'row',
+  //   justifyContent: 'space-between'
+  // },
+  // menu: {
+  //   height: 45,
+  //   width: 45
+  // },
+  // logo: {
+  //   height: 40,
+  //   width: 40
+  // },
+  // camera: {
+  //   height: 35,
+  //   width: 35
+  // },
   map: {
     backgroundColor: '#fdf8dc',
     height: 250,
