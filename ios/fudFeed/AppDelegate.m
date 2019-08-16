12f8a8558eb09ab9709c29e76a9ab053abaddfe7
Kristina Taing <kataing12@gmail.com>
2019-08-15 22:07:05 -0700

    [ops] updated configuration to integrate google maps api

--- ios/fudFeed/AppDelegate.m
+++ ios/fudFeed/AppDelegate.m
@@ -7,14 +7,17 @@
 
 #import "AppDelegate.h"
 
-#import <React/RCTBridge.h>
+//#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"API_TOKEN_HERE"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"fudFeed"
