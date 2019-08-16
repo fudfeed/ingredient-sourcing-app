e0806d699f345dd839ee06e922c6101dcb96a890
wayne hwang <whthwang@gmail.com>
2019-08-16 10:05:25 -0700

    [feat] implementing camera roll and camera - still WIP

--- app/components/input.js
+++ app/components/input.js
@@ -1,28 +1,172 @@
 import React, { Fragment } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, TouchableHighlight, AppRegistry, Button } from 'react-native';
+import { CameraRoll } from 'react-native';
 import Header from './header.js';
+import { Camera } from 'react-native-camera';
 
-const Input = (props) => {
-  return (
-    <Fragment>
+// const Input = (props) => {
+//   return (
+//     <Fragment>
+//       <SafeAreaView>
+//       <Header handleMenu={props.navigation.openDrawer} />
+//       <ScrollView stickyHeaderIndices={[1]} showsVerticalScrollIndicator={false}>
+//       </ScrollView>
+//       <View>
+//         <Text>This is Input</Text>
+//       </View>
+//       </SafeAreaView>
+//     </Fragment>
+//   );
+// };
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: []
+    };
+    this.handleButtonPress = this.handleButtonPress.bind(this);
+  }
+
+  handleButtonPress = () => {
+    CameraRoll.getPhotos({
+      first: 20,
+      assetType: 'Photos',
+    })
+      .then(allPhotos => {
+        this.setState({ photos: allPhotos.edges }, () => console.log('allPhotos:', this.state.photos));
+      })
+      .catch((err) => {
+        console.log('this is err', err);
+      });
+  };
+
+  render() {
+    return (
       <SafeAreaView>
-      <Header handleMenu={props.navigation.openDrawer} />
-      <ScrollView stickyHeaderIndices={[1]} showsVerticalScrollIndicator={false}>
-        <Text>This is Input</Text>
-      </ScrollView>
+        <View>
+          <Text>This is some shit</Text>
+        </View>
+        <View>
+          <Button onPress={this.handleButtonPress} title='photos'></Button>
+          <ScrollView>
+            {this.state.photos.map((photo, idx) => {
+              return (
+                <Image key={idx} source={{ uri: photo.node.image.uri }} />
+              )
+            })}
+          </ScrollView>
+        </View>
       </SafeAreaView>
-    </Fragment>
-  );
-};
+    )
+  }
+}
 
-export default Input;
+// class Input extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       cameraType: ''
+//     }
+//     this.takePicture = this.takePicture.bind(this);
+//   }
+
+//   takePicture() {
+//     const options = {};
+//     this.camera.capture({ metadata: options })
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+//   }
+
+//   render() {
+//     return (
+//       <View ref="cam"
+//         style={styles.container}>
+//         <Camera
+//         ref={cam => {this.cam = cam}}
+//         aspect={Camera.constants.Aspect.fill}>
+//           <View>
+//             <TouchableHighlight>
+//               <Text>Flip</Text>
+//             </TouchableHighlight>
+//             <TouchableHighlight>
+//               <Text onPress={this.takePicture}>Take</Text>
+//             </TouchableHighlight>
+//           </View>
+//         </Camera>
+//       </View>
+//     );
+//   }
+// };
+
+// export default class Input extends React.Component {
+//   render() {
+//     return (
+//       <View>
+//         <Camera
+//         ref={(cam) => {
+//           this.camera = cam
+//         }}
+//         >
+//         <Text onPress={this.takePicture.bind(this)}>
+//         [CAPTURE_IMAGE]
+//         </Text>
+//         </Camera>
+//       </View>
+//     )
+//   }
+
+//   takePicture() {
+//     const options =  {};
+//     this.camera.capture({metadata: options}).then((data) => {
+//       console.log(data);
+//     }).catch((error) => {
+//       console.log(error);
+//     })
+//   }
+// }
+// AppRegistry.registerComponent('cameraTutorial')
+
+/*
+      <Camera
+        ref="cam"
+        style={styles.container}>
+        <View>
+          <TouchableHighlight>
+            <Text>Flip</Text>
+          </TouchableHighlight>
+          <TouchableHighlight>
+            <Text>Take</Text>
+          </TouchableHighlight>
+        </View>
+      </Camera>
+*/
+
+// export default Input;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'column',
+//     backgroundColor: 'black',
+//   },
+//   preview: {
+//     flex: 1,
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   capture: {
+//     flex: 0,
+//     backgroundColor: '#fff',
+//     borderRadius: 5,
+//     padding: 15,
+//     paddingHorizontal: 20,
+//     alignSelf: 'center',
+//     margin: 20,
+//   }
+// })
+
+
 
 /*
 to use header
@@ -30,3 +174,5 @@ import Header from './header.js';
 <Header />
 <ScrollView stickyHeaderIndices={[1]} showsVerticalScrollIndicator={false}>
 */
+
+export default Input;
